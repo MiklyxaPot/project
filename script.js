217340cee404247906a256cb82a5dcaf3e848945
Mikhail <potaomg@gmail.com>
2022-06-21 22:04:31 +0300

    :логические операторы

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-// const answer = [];
-// answer[0] = prompt('как ваше имя?', "");
-// answer[1] = prompt('какая увас фамилия?', "");
-// answer[2] = prompt('сколько вам лет?', "18");
-// console.log(answer);
+// // const answer = [];
+// // answer[0] = prompt('как ваше имя?', "");
+// // answer[1] = prompt('какая увас фамилия?', "");
+// // answer[2] = prompt('сколько вам лет?', "18");
+// // console.log(answer);
 
-const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели??', '');
-const personaMovieDB = {
-   count: namberOfFilms,
-   movies: {
+// const namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели??', '');
+// const personaMovieDB = {
+//    count: namberOfFilms,
+//    movies: {
       
-   },
-   actors: {},
-   genres: [],
-   privat: false
-};
+//    },
+//    actors: {},
+//    genres: [],
+//    privat: false
+// };
 
-const a = prompt('один из последних просмотреных фильмов?', ''),
-      b = prompt('На сколько вы оцените его?', ''),
-      c =  prompt('один из последних просмотреных фильмов?', ''),
-      d = prompt('На сколько вы оцените его?', '');
+// const a = prompt('один из последних просмотреных фильмов?', ''),
+//       b = prompt('На сколько вы оцените его?', ''),
+//       c =  prompt('один из последних просмотреных фильмов?', ''),
+//       d = prompt('На сколько вы оцените его?', '');
 
-personaMovieDB.movies[a] = b;
-personaMovieDB.movies[c] = d;
+// personaMovieDB.movies[a] = b;
+// personaMovieDB.movies[c] = d;
 
-console.log(personaMovieDB);
-console.log();
+// console.log(personaMovieDB);
+// console.log();
 
 
+console.log(5 === 5 && 3> 1 || 5);
 
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+if (hamburger && cola || fries === 3 && nuggets ) {
+   console.log('Done');
+}
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
