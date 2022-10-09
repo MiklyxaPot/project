d27b385b9d889c4453873f91d13202a4c540523e
Mikhail <potaomg@gmail.com>
2022-10-09 16:05:50 +0300

    9/10

---
+++ практика по верстке/RealEstate/js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.slider-main-blok', {
+   // Optional parameters
+
+   loop: true,
+ 
+
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: 'slider-main-blok__arrow.swiper-button-next',
+     prevEl: 'slider-main-blok__arrow.swiper-button-prev',
+   },
+ 
+
+ });
---
+++ проэкт_Food/js/script.js
@@ -0,0 +1,145 @@
+window.addEventListener('DOMContentLoaded', () => {
+   // =================TABS===============
+   const tabs = document.querySelectorAll('.tabheader__item'),
+      tabsContent = document.querySelectorAll('.tabcontent'),
+      tabsParent = document.querySelector('.tabheader__items');
+
+   function hideTabContent() {
+      tabsContent.forEach(item => {
+         item.classList.add('hide');
+         item.classList.remove('show', 'fade');
+         // item.style.display = 'none';
+         // скрываем все классы 
+      });
+      tabs.forEach(item => {
+         item.classList.remove('tabheader__item_active');
+         // удаляем класс активности
+      });
+   }
+   function showTabContent(i = 0) {
+      // tabsContent[i].style.display = 'block';
+      tabsContent[i].classList.add('show', 'fade');
+      tabsContent[i].classList.remove('hide');
+
+      tabs[i].classList.add('tabheader__item_active');
+      // назначаем стиль блок и добавляем его к нужному блоку
+   }
+   // при начале открытия строницы  мы скрываем все блоки 
+   // и показываем нужный
+   hideTabContent();
+   showTabContent();
+   // если функция вызывается безаргумента по умочантюзапишится то что прописали вверху i = 0
+   tabsParent.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target && target.classList.contains('tabheader__item')) {
+         // при клике нужно определить к кокому номеру мы кликнули и  записатьв функцию показатьконтент
+         // это можно сделатьметобом перебора, 
+         tabs.forEach((item, i) => {
+            // item элемент который перебираем, i его номер
+            if (target == item) {
+               hideTabContent();
+               showTabContent(i);
+               //  если при переборе  item который совпал c target, то запускаются функции, и i номер 
+               // элемента записывается
+            }
+         });
+      }
+   });
+
+   // =========================TIMER====================
+
+   const deadLine = '2022-12-11';//дата конца
+
+   function getTimeRemaining(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date()); //от конечной датымы отнимаем текушюю
+         let days, hours, minutes, seconds;
+         if(t <= 0 ){
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+         } else {
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),// получаем дни полученное число делим на количествло милисикунд
+         hours = Math.floor((t / (1000 * 60 * 60) % 24)),// оператор % делит на число и возврашает остаток(50\24= 2.08 вернет 08)
+         minutes = Math.floor((t / 1000 / 60) % 60),//минуты это остаток от деления на 60
+         seconds = Math.floor((t / 1000) % 60);
+         }
+      
+      return {
+         'total': t,// total этосвойство ; тут записывается сколько время осталось в милисикундах
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      }
+   }
+
+   function getZero(num){
+      if(num >= 0 && num < 10){
+         return `0${num}`;
+      } else{
+         return num;
+      }
+   }
+   function setClock(selector, endtime) {
+      const timer = document.querySelector(selector),//к коому классу обрашаемся, сюдазапишится
+         days = document.querySelector('#days'),
+         hours = document.querySelector('#hours'),
+         minutes = document.querySelector('#minutes'),
+         seconds = document.querySelector('#seconds'),
+         timeInterval = setInterval(updateClock, 1000);// будет запускать эту функцию каждую секунду 
+         updateClock();
+      function updateClock() {
+         const t = getTimeRemaining(endtime);
+
+         days.innerHTML = getZero(t.days),
+         hours.innerHTML = getZero(t.hours),
+         minutes.innerHTML = getZero(t.minutes),
+         seconds.innerHTML = getZero(t.seconds);
+
+         if (t.total <= 0) {
+            clearInterval(timeInterval);//когда время выщло таймер остановится, не будет обновлятся
+         }
+
+      }
+
+   }
+   setClock('.timer', deadLine);
+
+      // =========================MODAL====================
+      // const modalOpen = document.querySelector('data-modal');
+      //    modalOpen.addEventListener('click', (e) =>{
+      //       e.target.style.display = "block";
+      //    })
+      const modalTrigger = document.querySelectorAll('[data-modal]');
+      const modal = document.querySelector('.modal');
+      const modalClose = document.querySelector('[data-close]');
+        
+
+      modalTrigger.forEach(btn =>{
+         btn.addEventListener('click', () =>{
+         modal.classList.add('show');
+         modal.classList.remove('hide');
+         document.body.style.overflow = 'hidden';
+      });
+      });
+      function closeModal(){
+         modal.classList.add('hide');
+         modal.classList.remove('show');
+         document.body.style.overflow = '';
+      }
+
+      modalClose.addEventListener('click', closeModal);
+
+      modal.addEventListener('click', (e) => {
+         if(e.target === modal){//когдапользователь кликнулна обвертку окна, но не само окно , то выполняется условие
+           closeModal();
+         }
+      })
+
+      document.addEventListener('keydown', (e) =>{
+         if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+         }
+      })
+});
